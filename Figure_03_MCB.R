c740ff7141981ede5aecf8df9989368f16c5f07c
Donia-212 <d2o0n0i3a@gmail.com>
2025-07-14 19:24:06 +0400

    Figure_03_MCB.R

--- Figure_03_MCB.R
+++ Figure_03_MCB.R
@@ -12,6 +12,6 @@ par(mgp = c(3, 1, 0))
 par(mar = c(5.1, 4.1, 4.1, 2.1))
 
 ##################################################### MCB RMSE ##################################################### 
-setwd('Forecasting_Climate_Policy_Uncertainty_Evidence_from_the_United_States/Dataset/MCB')
+setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/MCB')
 err <- read_excel('Results Combined X - RMSE.xlsx')
 tsutils::nemenyi(err, plottype = "mcb", main = 'MCB Plot for RMSE Metric', conf.level = 0.85)
