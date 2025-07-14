448c40388b86dd5983a133fb1ed9ff965fe3a8db
Donia-212 <d2o0n0i3a@gmail.com>
2025-07-14 13:50:59 +0400

    Create Figure_03_MCB.R

---
+++ Figure_03_MCB.R
@@ -0,0 +1,17 @@
+library(tsutils)
+library(PMCMRplus)
+library(vioplot)
+library(greybox)
+library(MTS)
+library(readxl)
+library(dplyr)
+library(murphydiagram)
+
+par(mfrow = c(1,1), mfcol = c(1,1))
+par(mgp = c(3, 1, 0))
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+
+##################################################### MCB RMSE ##################################################### 
+setwd('Forecasting-the-US-Climate-Policy-Uncertainty-using-Bayesian-Machine-Learning/Dataset/MCB')
+err <- read_excel('Results Combined X - RMSE.xlsx')
+tsutils::nemenyi(err, plottype = "mcb", main = 'MCB Plot for RMSE Metric', conf.level = 0.85)
